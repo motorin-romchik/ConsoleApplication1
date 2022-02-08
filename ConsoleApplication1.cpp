f31ee2bee915be22f2b805bcaa2a92ed3c987e62
mr_str <80831520+motorin-romchik@users.noreply.github.com>
2022-02-09 00:04:11 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "ru");
+	ifstream fun;
+	fun.open("Текст.txt");
+	if (!fun.is_open())
+	{
+		cout << "Ошибка открытия файла !"<<endl ;
+
+	}
+	else {
+		cout << "Файл открыт!" << endl;
+		char ch;
+		while (fun.get(ch))
+		{
+			cout << ch;
+
+		}
+	}
+
+	fun.close();
+
+	return 0;
+}
